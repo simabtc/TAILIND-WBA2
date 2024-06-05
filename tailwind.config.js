2d6b3624791b0e6d72517be0a0ba60e2d3aa5339
simabtc <skala.si.2022@skola.ssps.cz>
2024-06-05 17:42:42 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#7A9D8C",
+      secondary: "#ABA8BF",
+      text: "#050706"
+    },
+  },
+  plugins: [],
+}
+
+
